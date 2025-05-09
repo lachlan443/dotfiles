11a005960cd5c0c717296329e17a82f17448926c
lachlan443 <lachlan.banks1@gmail.com>
2025-05-09 11:13:40 +1000

    update script

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -3,10 +3,6 @@ git clone --bare https://github.com/lachlan443/dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME "$@"
 }
-mkdir -p .config-backup
 config config --local status.showUntrackedFiles no
-
-# Force checkout and overwrite existing files
-config checkout -f .  # This will force overwrite any conflicting files
-
+config checkout -f # Force checkout and overwrite existing files
 echo "Dotfiles install complete."
