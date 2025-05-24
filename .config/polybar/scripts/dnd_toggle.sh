8740af3bd55abea4cfbdad29e006edc86712f68b
lachlan443 <lachlan.banks1@gmail.com>
2025-05-24 12:25:19 +1000

    polybar: update battery

--- .config/polybar/scripts/dnd_toggle.sh
+++ .config/polybar/scripts/dnd_toggle.sh
@@ -4,6 +4,8 @@ level=$(dunstctl get-pause-level)
 
 if [ "$level" -ge 10 ]; then
     dunstctl set-pause-level 0
+    # dunstify "Do Not Disturb" "Disabled"
 else
     dunstctl set-pause-level 10
+    # dunstify "Do Not Disturb" "Enabled"
 fi
