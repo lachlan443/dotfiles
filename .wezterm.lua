a623bb9350e68774d4914fb49bc9aec7d87f0fe4
Lachlan Banks <87565923+lachlan443@users.noreply.github.com>
2025-10-24 13:47:00 +1100

    macos opt+arrow

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,4 +13,25 @@ config.colors = {
    foreground = "#FFFFFF", -- white font color
 }
 
+
+local act = wezterm.action
+
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' },
+  },
+}
+
+
 return config
